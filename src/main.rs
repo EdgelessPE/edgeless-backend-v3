1614d1d5009953b3daf92e4558dbd2bedfa8c833
Hydrogen <3038094028@qq.com>
2022-08-17 21:32:41 +0800

    fix: error

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,28 @@ use std::{collections::HashMap, sync::mpsc::channel, thread::spawn};
 use crate::config::read_config;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use response_collector::ResponseCollector;
+use lazy_static::lazy_static;
+use std::sync::{Arc, Mutex};
+
+lazy_static! {
+    static ref COLLECTOR: Arc<Mutex<Option<ResponseCollector>>> = Arc::new(Mutex::new(None));
+}
+
+async fn handler() -> HttpResponse {
+    let collector = &mut *COLLECTOR.lock().unwrap();
+    if let Some(collector) = collector.as_mut() {
+        match collector.ept() {
+            Ok(res) => HttpResponse::Ok().json(res),
+            Err(e) =>  {
+                println!("Error:Can't collect ept response {:?}",e);
+                HttpResponse::InternalServerError().body("Can't collect ept response")
+            }
+        }
+    } else {
+        println!("Error:Can't collect ept response: Uninit");
+        HttpResponse::InternalServerError().body("Can't collect ept response")
+    }
+}
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -31,7 +53,9 @@ async fn main() -> std::io::Result<()> {
         cfg.position.plugins.clone(),
     );
 
-    let mut response_collector = ResponseCollector::new(result_receiver, cmd_sender, cfg.clone());
+    let mut collector = COLLECTOR.lock().unwrap();
+
+    *collector = Some(ResponseCollector::new(result_receiver, cmd_sender, cfg.clone()));
 
     //启动 daemon 服务
     spawn(move || {
@@ -40,15 +64,7 @@ async fn main() -> std::io::Result<()> {
     });
 
     HttpServer::new(|| {
-        App::new().route("/v3/ept", web::get().to(|| {
-            let res = response_collector.ept();
-            if let Err(e) = res {
-                println!("Error:Can't collect ept response {:?}",e);
-                HttpResponse::InternalServerError().body("Can't collect ept response")
-            } else {
-                HttpResponse::Ok().json(res.unwrap())
-            }
-        }))
+        App::new().route("/v3/ept", web::get().to(handler))
         // .service(ept)
     })
     .bind(("127.0.0.1", 8080))?
