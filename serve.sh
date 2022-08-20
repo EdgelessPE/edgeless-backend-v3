51a8ea13faabbe8dfdea8004d7e4fc6deabecda1
Cno <dsyourshy@qq.com>
2022-08-20 21:45:11 +0800

    增加serve脚本

---
+++ serve.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export BUILD_ID=dontKillMe
+git restore .
+git pull
+source $HOME/.cargo/env
+cargo build --release
+
+if [ $? -ne 0 ]
+then
+	echo Error:Build failed,exit
+	exit 1
+fi
+
+pid=`ps -ef|grep edgeless-backend|grep -v grep|awk '{print $2}'`
+if [ -n "$pid" ]
+then 
+	echo "kill " $pid
+	kill -9 $pid
+fi
+
+if [ -e "nohup.out" ]
+then
+	rm nohup.out
+fi
+
+nohup ./target/release/edgeless-backend&
